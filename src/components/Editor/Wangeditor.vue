5fa951de092ed2a49b42a7ea0bf11fba02509b4f
hm243695czl <1224311695@qq.com>
2022-07-11 15:29:43 +0800

    优化富文本编辑器

--- src/components/Editor/Wangeditor.vue
+++ src/components/Editor/Wangeditor.vue
@@ -1,32 +1,17 @@
 <template>
 	<div class='wang-editor-container'>
-		<Toolbar
-			style="border-bottom: 1px solid #ccc"
-			:editor="editorRef"
-			:defaultConfig="toolbarConfig"
-			:mode="mode"
-		/>
-		<Editor
-			:style="editorStyle"
-			v-model="contentHtml"
-			:defaultConfig="editorConfig"
-			:mode="mode"
-			@onCreated="handleCreated"
-			@onBlur='onBlur'
-		/>
+		<div id="toolbar-container"></div>
+		<div id="editor-container" :style='{height: height + "px", overflow: "hidden"}'></div>
 	</div>
 </template>
 
 <script lang='ts'>
 import '@wangeditor/editor/dist/css/style.css';
-import { defineComponent, shallowRef, ref, onBeforeUnmount } from 'vue';
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
+import { defineComponent, onMounted } from 'vue';
+import { createEditor, createToolbar, IEditorConfig, IDomEditor, IToolbarConfig } from '@wangeditor/editor';
 
 export default defineComponent({
 	name: 'HlEditor',
-	components: {
-		Editor,Toolbar
-	},
 	props: {
 		content: {
 			type: String
@@ -41,43 +26,45 @@ export default defineComponent({
 		}
 	},
 	setup (props, ctx) {
-		const editorRef = shallowRef();
-		const toolbarConfig = {
+		const initEditor = () => {
+			// 编辑器配置
+			const editorConfig: Partial<IEditorConfig> = {};
+			editorConfig.placeholder = '请输入...';
+			editorConfig.readOnly = props.readonly;
+			editorConfig.onBlur = (editor: IDomEditor) => {
+				ctx.emit('editor-blur', editor.getHtml())
+			};
+			editorConfig.MENU_CONF = {
+				uploadImage: {
+					filedName: 'file',
+					base64LimitSize: 10 * 1024 * 1024 // 10M 以下插入base64
+				}
+			}
 
-		};
-		const contentHtml = ref(props.content);
-		const mode = 'default';
-		const editorConfig = {
-			placeholder: '请输入内容...',
-			readOnly: props.readonly,
-			isFullScreen: false
-		};
-		// 组件销毁时，也及时销毁编辑器
-		onBeforeUnmount(() => {
-			const editor = editorRef.value
-			if (editor == null) return
-			editor.destroy()
+			// 工具栏配置
+			const toolbarConfig: Partial<IToolbarConfig> = {
+				excludeKeys: ['fullScreen'],
+			};
+
+			// 创建编辑器
+			const editor = createEditor({
+				selector: '#editor-container',
+				html: props.content,
+				config: editorConfig,
+				mode: 'default',
+			});
+
+			// 创建工具栏
+			const toolbar = createToolbar({
+				editor,
+				selector: '#toolbar-container',
+				config: toolbarConfig,
+				mode: 'default',
+			});
+		}
+		onMounted(() => {
+			initEditor();
 		});
-		const handleCreated = (editor: any) => {
-			editorRef.value = editor // 记录 editor 实例，重要！
-		};
-		const onBlur = (editor: any) => {
-			ctx.emit('editor-blur', editor.getHtml())
-		};
-		const editorStyle = {
-			height:  props.height + 'px',
-			overflow: 'hidden'
-		}
-		return {
-			editorRef,
-			toolbarConfig,
-			mode,
-			contentHtml,
-			editorConfig,
-			handleCreated,
-			onBlur,
-			editorStyle
-		}
 	}
 });
 </script>
