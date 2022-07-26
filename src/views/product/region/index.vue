5678c5c89c10c2be1c49ff44b0be115bd7480336
hm243695czl <1224311695@qq.com>
2022-07-26 23:27:41 +0800

    完善【行政区域】

--- src/views/product/region/index.vue
+++ src/views/product/region/index.vue
@@ -1,6 +1,17 @@
 <template>
 	<div class='product-region-container'>
-		行政区域
+		<el-card shadow="hover">
+			<el-table :data='dataList' row-key='id' :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
+				<el-table-column prop='id' label='区域id' />
+				<el-table-column prop='code' label='区域编码' />
+				<el-table-column prop='value' label='区域名称' />
+				<el-table-column prop='level' label='区域类型'>
+					<template #default='scope'>
+						<el-tag>{{levelObj[scope.row.level]}}</el-tag>
+					</template>
+				</el-table-column>
+			</el-table>
+		</el-card>
 	</div>
 </template>
 
@@ -13,12 +24,17 @@ export default defineComponent({
 	name: 'productRegion',
 	setup() {
 		const state = reactive({
-			dataList: []
+			dataList: [],
+			levelObj: {
+				0: '省',
+				1: '市',
+				2: '县'
+			}
 		});
 		const getRegionList = () => {
 			getRegionListApi().then(res => {
 				if (res.status === StatusEnum.SUCCESS) {
-					console.log(res);
+					state.dataList = res.data;
 				}
 			})
 		};
