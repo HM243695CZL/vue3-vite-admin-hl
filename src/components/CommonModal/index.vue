277d5696650b12b84c90044b214b0beb423c57b2
hm243695czl <1224311695@qq.com>
2022-12-15 11:36:11 +0800

    优化公共弹窗组件

--- src/components/CommonModal/index.vue
+++ src/components/CommonModal/index.vue
@@ -1,71 +1,81 @@
 <template>
-	<div class='common-modal-container'>
-		<el-dialog :append-to-body='false'
-							 :close-on-click-modal='false'
-							 :title='title'
-							 v-model='isShowDialog' :width='props.modalWidth'>
-			<slot></slot>
-			<template #footer>
-				<div class='dialog-footer'>
-					<el-button @click='closeDialog'>取 消</el-button>
-					<el-button type='primary' @click='clickConfirm'>确 定</el-button>
-				</div>
-			</template>
-		</el-dialog>
-	</div>
+  <div class='common-modal-container'>
+    <el-dialog :append-to-body='false'
+               :close-on-click-modal='false'
+               :title='title'
+               v-model='isShowDialog' :width='props.modalWidth'>
+      <slot></slot>
+      <template #footer>
+        <div class='dialog-footer'>
+          <el-button @click='closeDialog'>取 消</el-button>
+          <el-button type='primary' @click='clickConfirm'>确 定</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script lang='ts' setup>
 import useCommonModal from '/@/hooks/useCommonModal';
 
 const props = defineProps({
-	title: {
-		type: String,
-		default: ''
-	},
-	createPath: {
-		type: String,
-		required: true
-	},
-	updatePath: {
-		type: String,
-		required: true
-	},
-	viewPath: {
-		type: String,
-		required: true
-	},
-	ruleForm: {
-		type: Object,
-		default: () => {}
-	},
-	modalWidth: {
-		type: String,
-		default: '600px'
-	}
+  title: {
+    type: String,
+    default: ''
+  },
+  createPath: {
+    type: String,
+    required: true
+  },
+  updatePath: {
+    type: String,
+    required: true
+  },
+  viewPath: {
+    type: String,
+    required: true
+  },
+  ruleForm: {
+    type: Object,
+    default: () => {}
+  },
+  modalWidth: {
+    type: String,
+    default: '600px'
+  },
+  isFormCreate: {
+    type: Boolean,
+    default: false
+  },
+  clearFields: {
+    type: Array as any,
+    default: () => []
+  }
 });
 const emits = defineEmits([
-	'refreshList'
+  'refreshList'
 ]);
 
 const refreshList = () => {
-	emits('refreshList');
+  emits('refreshList');
 };
 
 const {
-	openDialog,
-	closeDialog,
-	clickConfirm,
-	isShowDialog,
-	title,
+  openDialog,
+  closeDialog,
+  clickConfirm,
+  isShowDialog,
+  title,
 } = useCommonModal({
-	createPath: props.createPath,
-	updatePath: props.updatePath,
-	viewPath: props.viewPath,
-	title: props.title,
-	refreshList,
+  createPath: props.createPath,
+  updatePath: props.updatePath,
+  viewPath: props.viewPath,
+  title: props.title,
+  refreshList,
+  clearFields: props.clearFields,
+  isFormCreate: props.isFormCreate
 });
 defineExpose({
-	openDialog,
+  openDialog,
 });
 </script>
--- src/hooks/useCommonModal.ts
+++ src/hooks/useCommonModal.ts
@@ -20,17 +20,21 @@ interface IInitModal {
 	refreshList: Function, // 更新列表的方法
 	otherForm?: any, // 其他表单属性
 	otherInitMethod?: Function, // 运行其他方法
+	clearFields?: Array<String>, // 需要清除校验结果的字段
+	isFormCreate?: boolean, // 是否表单创建
 }
 
 export default function({
-		createPath,
-		updatePath,
-		viewPath,
-		title,
-		refreshList,
-		otherForm,
-		otherInitMethod,
-	}: IInitModal) {
+							createPath,
+							updatePath,
+							viewPath,
+							title,
+							refreshList,
+							otherForm,
+							otherInitMethod,
+							clearFields,
+							isFormCreate
+						}: IInitModal) {
 	const state = reactive({
 		isShowDialog: false,
 		title: '',
@@ -38,6 +42,7 @@ export default function({
 			id: '',
 		},
 		formModalRef: {} as any, // 表单实例
+		formMap: {}
 	});
 	// 回显表单数据
 	const setFormValue = formData => {
@@ -51,20 +56,35 @@ export default function({
 		state.isShowDialog = false;
 	};
 	const openDialog = ({
-			dataId,
-			childRef,
-		}: IOpenModal) => {
+							dataId,
+							childRef,
+						}: IOpenModal) => {
 		state.isShowDialog = true;
 		state.ruleForm.id = '';
 		nextTick(() => {
 			state.formModalRef = childRef.value;
-			state.formModalRef.formRef.resetFields();
+			if (isFormCreate) {
+				state.formModalRef.state.fApi.resetFields();
+				clearFields?.map(item => {
+					state.formModalRef.state.fApi.clearValidateState(item);
+				});
+			} else {
+				state.formModalRef.formRef.resetFields();
+			}
 			if (dataId) {
 				state.ruleForm.id = dataId;
 				state.title = '修改' + title;
 				getAction(`${viewPath}/${dataId}`, '').then(res => {
 					if (res.status === StatusEnum.SUCCESS) {
-						setFormValue(res.data);
+						if (isFormCreate) {
+							let objValue = {};
+							state.formModalRef.state.fApi.fields().map(item => {
+								objValue[item] = res.data[item];
+							});
+							state.formModalRef.state.fApi.coverValue(objValue);
+						} else {
+							setFormValue(res.data);
+						}
 						if (otherInitMethod) {
 							otherInitMethod(res.data);
 						}
@@ -79,21 +99,40 @@ export default function({
 		});
 	};
 	const clickConfirm = () => {
-		state.formModalRef.formRef.validate((valid: boolean) => {
-			if (valid) {
-				postAction(state.ruleForm.id ? updatePath : createPath, {
-					...state.ruleForm,
-					...state.formModalRef.state.ruleForm,
-					...otherForm,
-				}).then(res => {
-					if (res.status === StatusEnum.SUCCESS) {
-						ElMessage.success(res.message);
-						closeDialog();
-						refreshList();
-					}
-				});
-			}
-		});
+		if (isFormCreate) {
+			state.formModalRef.state.fApi.validate(valid => {
+				if (valid === true) {
+					postAction(state.ruleForm.id ? updatePath : createPath, {
+						...state.formModalRef.state.fApi.formData(),
+						id: state.ruleForm.id
+					}).then(res => {
+						if (res.status === StatusEnum.SUCCESS) {
+							ElMessage.success(res.message);
+							closeDialog();
+							refreshList && refreshList();
+						}
+					})
+				} else {
+					ElMessage.error('请完善必填项');
+				}
+			})
+		} else {
+			state.formModalRef.formRef.validate((valid: boolean) => {
+				if (valid) {
+					postAction(state.ruleForm.id ? updatePath : createPath, {
+						...state.ruleForm,
+						...state.formModalRef.state.ruleForm,
+						...otherForm,
+					}).then(res => {
+						if (res.status === StatusEnum.SUCCESS) {
+							ElMessage.success(res.message);
+							closeDialog();
+							refreshList();
+						}
+					});
+				}
+			});
+		}
 	};
 	return {
 		...toRefs(state),
